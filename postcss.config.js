73b05aa3c6227d617a22222d9f0600415dd6cc83
JoeLucas99 <josephlucas9315@gmail.com>
2025-03-17 14:47:58 -0400

    It Just works

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
+// PostCSS configuration for processing CSS
+// This file configures PostCSS to use Tailwind CSS and Autoprefixer
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: {}, // Enable Tailwind CSS processing
+    autoprefixer: {}, // Enable automatic vendor prefixing
   },
 }
 
